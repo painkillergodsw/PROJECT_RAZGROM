148e78d1796afb5830b9728ef16ed64e0d2b7976
painkillergodsw <pgod.sw@gmail.com>
2025-06-13 09:20:11 +0300

    sm fx

--- app/services/auth_service/config.py
+++ app/services/auth_service/config.py
@@ -20,9 +20,6 @@ class DBSetting(BaseSettings):
 
     @property
     def conn_url(self) -> str:
-        print(
-            f"mysql+aiomysql://{self.DB_USER}:{self.DB_PWD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-        )
         return f"mysql+aiomysql://{self.DB_USER}:{self.DB_PWD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
 

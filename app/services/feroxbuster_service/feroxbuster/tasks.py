9510ee60e1bec13fbf5a042c5a5c87d88b947260
painkillergodsw <pgod.sw@gmail.com>
2026-07-24 01:41:42 +0300

    1 таска для feroxbuster для нескольких доменов

--- app/services/feroxbuster_service/feroxbuster/tasks.py
+++ app/services/feroxbuster_service/feroxbuster/tasks.py
@@ -6,8 +6,8 @@ logger = l.getLogger(__name__)
 feroxbuster = SDK()
 
 async def scan_domain(domain: str):
-    logger.info(f"Feroxbuster начало скана: {domain}")
+    print(f"Feroxbuster начало скана: {domain}")
     result = await feroxbuster.scan_domain(domain)
-    logger.info(f"Feroxbuster скан окончен : {domain}")
+    print(f"Feroxbuster скан окончен : {domain}")
 
     return result

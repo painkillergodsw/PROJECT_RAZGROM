987cb05e0f53d87929b031cec4e21ba1b03bf647
painkillergodsw <pgod.sw@gmail.com>
2026-08-12 00:32:08 +0300

    MVP nuclei service + перенес dast сервисы в отдельный пакет

---
+++ app/services/dast/nuclei_service/README.md
@@ -0,0 +1,1 @@
+Active DAST scan
---
+++ app/services/dast/nuclei_service/consumer.py
@@ -0,0 +1,18 @@
+from nuclei.tasks import scan_domains
+from config import config
+from common.kafka.consumer import base_create_topics, base_consume
+
+async def create_topics():
+    await base_create_topics(config)
+
+async def consume(producer):
+    await base_consume(producer, config, handle_msg=handle_msg)
+
+async def handle_msg(producer, msg):
+
+    try:
+        result = await scan_domains(msg.value["domains"])
+        await producer.send(config.kafka.PRODUCE_T, result, msg.key)
+
+    except Exception as e:
+        print(str(e))
